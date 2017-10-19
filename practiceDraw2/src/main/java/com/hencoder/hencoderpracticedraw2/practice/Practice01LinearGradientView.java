bcbc08d4dee1a52b9cb26f5ad6fd2bccde01b0a3
yang.jianan <364804051@qq.com>
2017-10-19 14:05:46 +0800

    未读信息提示数目 View

--- practiceDraw2/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
+++ practiceDraw2/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
@@ -1,17 +1,22 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.LinearGradient;
 import android.graphics.Paint;
-import android.graphics.Shader;
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
 
 public class Practice01LinearGradientView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
 
     public Practice01LinearGradientView(Context context) {
         super(context);
@@ -26,18 +31,43 @@ public class Practice01LinearGradientView extends View {
     }
 
     {
-        // 用 Paint.setShader(shader) 设置一个 LinearGradient
-        // LinearGradient 的参数：坐标：(100, 100) 到 (500, 500) ；颜色：#E91E63 到 #2196F3
-        // 线性渐变
-        Shader shader = new LinearGradient(100,100,500,500, Color.parseColor("#E91E63"),
-                Color.parseColor("#2196F3"), Shader.TileMode.CLAMP);
-        paint.setShader(shader);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(300, 300, 200, paint);
+        int count = 66;
+
+        bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        int textureWidth = bitmap.getWidth();
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+        Log.d("textureWidth", "textureWidth = " + textureWidth);
+        paint.setColor(Color.RED);
+        canvas.drawCircle(textureWidth - 17 - 6, 16 + 6, 16 + 6, paint);
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(2);
+        canvas.drawCircle(textureWidth - 17 - 6, 16 + 6, 16 + 6, paint);
+
+        Paint countPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DEV_KERN_TEXT_FLAG);
+        countPaint.setColor(Color.WHITE);
+        countPaint.setTextSize(26f);
+        countPaint.setTypeface(Typeface.DEFAULT_BOLD);
+
+        float x = textureWidth - 24 - 4;
+        if (count > 9) x -= 4 + 6;
+
+        if (count > 99) {
+            countPaint.setTextSize(22f);
+            String text = String.valueOf(99) + "+";
+            canvas.drawText(text, x - 2, 25 + 5, countPaint);
+        } else {
+            String text = String.valueOf(count);
+            canvas.drawText(text, x, 25 + 5, countPaint);
+        }
     }
 }
